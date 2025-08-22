52550e689a145f6e730b8a927bfb3bb9843b87b1
DovaPT <peterd5590@gmail.com>
2025-08-22 14:25:28 +0200

    made it easier to read Display impl for Type added Type.color() for color code creation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{fmt::{Debug, Display}, fs::read_to_string};
+use std::{
+    fmt::{Debug, Display},
+    fs::read_to_string,
+};
 
 use serde::{Deserialize, Serialize};
 use serde_json::Result;
@@ -25,38 +28,61 @@ enum Type {
     Steel,
 }
 
+impl Type {
+    fn color(&self) -> &'static str {
+        match self {
+            Self::Fire => "\x1b[48;2;255;129;48;38;2;240;240;240m",
+            Self::Grass => "\x1b[48;2;112;192;80;38;2;240;240;240m",
+            Self::Water => "\x1b[48;2;48;144;240;38;2;240;240;240m",
+            Self::Poison => "\x1b[48;2;160;80;144;38;2;240;240;240m",
+            Self::Bug => "\x1b[48;2;160;176;32;38;2;240;240;240m",
+            Self::Normal => "\x1b[48;2;160;160;144;38;2;240;240;240m",
+            Self::Flying => "\x1b[48;2;128;144;240;38;2;240;240;240m",
+            Self::Dark => "\x1b[48;2;112;80;64;38;2;240;240;240m",
+            Self::Dragon => "\x1b[48;2;112;96;224;38;2;240;240;240m",
+            Self::Rock => "\x1b[48;2;176;160;96;38;2;240;240;240m",
+            Self::Ground => "\x1b[48;2;208;176;80;38;2;240;240;240m",
+            Self::Ice => "\x1b[48;2;96;192;240;38;2;240;240;240m",
+            Self::Psychic => "\x1b[48;2;240;80;144;38;2;240;240;240m",
+            Self::Fairy => "\x1b[48;2;224;144;224;38;2;240;240;240m",
+            Self::Ghost => "\x1b[48;2;96;96;176;38;2;240;240;240m",
+            Self::Fighting => "\x1b[48;2;176;80;64;38;2;240;240;240m",
+            Self::Electric => "\x1b[48;2;240;192;48;38;2;240;240;240m",
+            Self::Steel => "\x1b[48;2;160;160;176;38;2;240;240;240m",
+        }
+    }
+}
 
 impl Display for Type {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::Fire => write!(f, "\x1b[48;2;{r};{g};{b}m Fire \x1b[0m",r = 0xFF, g = 0x81, b = 0x30),
-            Self::Grass => write!(f, "\x1b[48;2;{r};{g};{b};38;2;0;0;0m Grass \x1b[0m", r = 0x7A, g = 0xC7, b = 0x4C),
-            Self::Water => write!(f, "\x1b[48;2;{r};{g};{b};m Water \x1b[0m", r = 0x63, g = 0x90, b = 0xF0),
-            Self::Poison => write!(f, "\x1b[48;2;{r};{g};{b}m Poison \x1b[0m", r = 0xA3, g = 0x3E, b = 0xA1),
-            Self::Bug => write!(f, "\x1b[48;2;{r};{g};{b};38;2;0;0;0m Bug \x1b[0m", r = 0xA6, g = 0xB9, b = 0x1A),
-            Self::Normal => write!(f, "\x1b[48;2;{r};{g};{b};38;2;0;0;0m Normal \x1b[0m", r = 0xA8, g = 0xA7, b = 0x7A),
-            Self::Flying => write!(f, "\x1b[48;2;{r};{g};{b};38;2;0;0;0m Flying \x1b[0m", r = 0xA9, g = 0x8F, b = 0xF3),
-            Self::Dark => write!(f, "\x1b[48;2;{r};{g};{b};m Dark \x1b[0m", r = 0x70, g = 0x50, b = 0x40),
-            Self::Dragon => write!(f, "\x1b[48;2;{r};{g};{b};m Dragon \x1b[0m", r = 0x6F, g = 0x35, b = 0xFC),
-            Self::Rock => write!(f, "\x1b[48;2;{r};{g};{b};38;2;0;0;0m Rock \x1b[0m", r = 0xB6, g = 0xA1, b = 0x36),
-            Self::Ground => write!(f, "\x1b[48;2;{r};{g};{b};38;2;0;0;0m Ground \x1b[0m", r = 0xE2, g = 0xBF, b = 0x65),
-            Self::Ice => write!(f, "\x1b[48;2;{r};{g};{b};38;2;0;0;0m Ice \x1b[0m", r = 0x96, g = 0xD9, b = 0xD6),
-            Self::Psychic => write!(f, "\x1b[48;2;{r};{g};{b};38;2;0;0;0m Psychic \x1b[0m", r = 0xF9, g = 0x55, b = 0x87),
-            Self::Fairy => write!(f, "\x1b[48;2;{r};{g};{b};38;2;0;0;0m Fairy \x1b[0m", r = 0xE0, g = 0x90, b = 0xE0),
-            Self::Ghost => write!(f, "\x1b[48;2;{r};{g};{b};m Ghost \x1b[0m", r = 72, g = 72, b = 157),
-            Self::Fighting => write!(f, "\x1b[48;2;{r};{g};{b};m Fighting \x1b[0m", r = 0xB0, g = 0x50, b = 0x40),
-            Self::Electric => write!(f, "\x1b[48;2;{r};{g};{b};38;2;0;0;0m Electric \x1b[0m", r = 0xEE, g = 0xD5, b = 0x35),
-            Self::Steel => write!(f, "\x1b[48;2;{r};{g};{b};38;2;0;0;0m Steel \x1b[0m", r = 0xB7, g = 0xB7, b = 0xCE),
+            Self::Fire => write!(f, "{} Fire \x1b[0m", self.color()),
+            Self::Grass => write!(f, "{} Grass \x1b[0m", self.color()),
+            Self::Water => write!(f, "{} Water \x1b[0m", self.color()),
+            Self::Poison => write!(f, "{} Poison \x1b[0m", self.color()),
+            Self::Bug => write!(f, "{} Bug \x1b[0m", self.color()),
+            Self::Normal => write!(f, "{} Normal \x1b[0m", self.color()),
+            Self::Flying => write!(f, "{} Flying \x1b[0m", self.color()),
+            Self::Dark => write!(f, "{} Dark \x1b[0m", self.color()),
+            Self::Dragon => write!(f, "{} Dragon \x1b[0m", self.color()),
+            Self::Rock => write!(f, "{} Rock \x1b[0m", self.color()),
+            Self::Ground => write!(f, "{} Ground \x1b[0m", self.color()),
+            Self::Ice => write!(f, "{} Ice \x1b[0m", self.color()),
+            Self::Psychic => write!(f, "{} Psychic \x1b[0m", self.color()),
+            Self::Fairy => write!(f, "{} Fairy \x1b[0m", self.color()),
+            Self::Ghost => write!(f, "{} Ghost \x1b[0m", self.color()),
+            Self::Fighting => write!(f, "{} Fighting \x1b[0m", self.color()),
+            Self::Electric => write!(f, "{} Electric \x1b[0m", self.color()),
+            Self::Steel => write!(f, "{} Steel \x1b[0m", self.color()),
         }
-        
     }
 }
 
 #[derive(Serialize, Deserialize)]
 #[serde(untagged)]
 enum Typing {
-    Single([Type;1]),
-    Double([Type;2])
+    Single([Type; 1]),
+    Double([Type; 2]),
 }
 
 impl Debug for Typing {
@@ -65,15 +91,14 @@ impl Debug for Typing {
     }
 }
 
-impl Display for Typing{
+impl Display for Typing {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self{
+        match self {
             Self::Single(t) => write!(f, "{}", t[0]),
-            Self::Double(t) => write!(f, "{}, {}", t[0], t[1])
+            Self::Double(t) => write!(f, "{}, {}", t[0], t[1]),
         }
     }
 }
-
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Pokemon {
